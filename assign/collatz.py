3962e48067cab5d7b04eabb6429b8c269f6b35b7
Ivan Tam <ivangonekrazy@gmail.com>
2011-07-11 22:08:02 -0700

    added refernce implementation of collatz sequence generator

---
+++ assign/collatz.py
@@ -0,0 +1,18 @@
+def collatz(num):
+    
+    while num > 0:
+        print num
+
+        if num == 1:
+            break
+
+        if num % 2 == 1:
+            num = num * 3 + 1
+        elif num % 2 == 0:
+            num = num / 2
+
+if __name__ == "__main__":
+    #collatz(1024)
+    #collatz(50)
+    #collatz(1)
+    collatz(0)
