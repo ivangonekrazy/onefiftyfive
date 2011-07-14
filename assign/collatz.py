3727b014bf26eeaa24130f0190a82fa821d1bb63
Ivan Tam <ivangonekrazy@gmail.com>
2011-07-14 08:59:14 -0700

    added doctests to collatz and made it return a list instead of just printing

--- assign/collatz.py
+++ assign/collatz.py
@@ -1,7 +1,25 @@
 def collatz(num):
+    """
+    Given a starting number, return the
+    Collatz sequence as a list.
+
+    >>> collatz(50)
+    [50, 25, 76, 38, 19, 58, 29, 88, 44, 22, 11, 34, 17, 52, 26, 13, 40, 20, 10, 5, 16, 8, 4, 2, 1]
+
+    >>> collatz(16)
+    [16, 8, 4, 2, 1]
+    
+    >>> collatz(10)
+    [10, 5, 16, 8, 4, 2, 1]
+
+    >>> collatz(0)
+    []
+
+    """
+    sequence = []
     
     while num > 0:
-        print num
+        sequence.append(num)
 
         if num == 1:
             break
@@ -11,9 +29,8 @@ def collatz(num):
         elif num % 2 == 0:
             num = num / 2
 
+    return sequence
+
 if __name__ == "__main__":
-    #collatz(1024)
-    #collatz(50)
-    collatz(10)
-    #collatz(1)
-    #collatz(0)
+    import doctest
+    doctest.testmod(verbose=True)
