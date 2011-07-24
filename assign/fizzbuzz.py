007266b45256d6204a6f66206bc08d2da5531caa
Ivan Tam <ivan@hipnik.net>
2011-07-24 16:31:30 -0700

    made fizzbuzz a function

--- assign/fizzbuzz.py
+++ assign/fizzbuzz.py
@@ -1,9 +1,28 @@
-for n in range(1,21):
-    if n % 3 == 0 and n % 5 == 0:
-        print n, "FizzBuzz"
-    elif n % 3 == 0:
-        print n, "Fizz"
-    elif n % 5 == 0:
-        print n, "Buzz"
-    else:
-        print n
+def fizzbuzz():
+    """
+        For each number in the range (1,100),
+        print 'Fizz' if the number if a multiple of 3
+        print 'Buzz' if the number if a multiple of 5
+        print 'FizzBuzz' if the number if a multiple of 3 and 5
+        otherwise, just print the number
+    """
+
+    for n in range(1,101):
+
+        # multiple of 3 and 5
+        if n % 3 == 0 and n % 5 == 0:
+            print "FizzBuzz"
+
+        # multiple of 3
+        elif n % 3 == 0:
+            print "Fizz"
+
+        # multiple of 5
+        elif n % 5 == 0:
+            print "Buzz"
+
+        # anything else
+        else:
+            print n
+
+fizzbuzz()
